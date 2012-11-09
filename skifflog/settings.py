bc6b8ed93cd6fe9ee8001d9dd32c25fe22e99c18
James Cleveland <jamescleveland@gmail.com>
2012-11-09 13:09:20 +0000

    django-require amazingness

--- skifflog/settings.py
+++ skifflog/settings.py
@@ -56,7 +56,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -80,8 +80,18 @@ STATICFILES_FINDERS = (
 STATICFILES_STORAGE = 'require.storage.OptimizedStaticFilesStorage'
 
 REQUIRE_BASE_URL = "javascripts"
+REQUIRE_BUILD_PROFILE = None
 REQUIRE_JS = "require.js"
-REQUIRE_DEBUG = settings.DEBUG
+REQUIRE_DEBUG = False
+REQUIRE_STANDALONE_MODULES = {
+    "main": {
+        # Where to output the built module, relative to REQUIRE_BASE_URL.
+        "out": "main-built.js",
+
+        # Optional: A build profile used to build this standalone module.
+        "build_profile": "main.build.js",
+    }
+}
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '8g3%2m46_&amp;xv=r)a1)1jxjwo98ln%641fabi6_s!e8$d&amp;%8df4'
